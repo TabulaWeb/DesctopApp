b526308139b00dd591220372dc29e7b791c1f303
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-10-14 20:26:10 +0300

    header

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+const ipc = ipcMain
 
 function createWindow () {
     const win = new BrowserWindow({
@@ -8,17 +9,44 @@ function createWindow () {
       minWidth: 940,
       minHeight: 560,
       frame: false,
-      backgroundColor: 'white',
+      backgroundColor: '#0f0e17',
       webPreferences: {
         nodeIntegration: true,
         worldSafeExecuteJavaScript: true,
-        contextIsolation: true,
+        contextIsolation: false,
         devTools: true,
         preload: path.join(__dirname, 'preload.js')
       }
     })
   
     win.loadFile('static/index.html')
+
+    // MAXIMIZE APP
+    ipc.on('maximizeRestoreApp', () => {
+      if(win.isMaximized()){
+        win.restore()
+      } else {
+        win.maximize()
+      }
+    })
+    // Check if is Maximized
+    win.on('maximize', () => {
+      win.webContents.send('isMaximized')
+    })
+    // Check if is Restored
+    win.on('unmaximize', () => {
+      win.webContents.send('isRestored')
+    })
+
+    // MINIMIZE APP
+    ipc.on('minimazeApp', () => {
+      win.minimize()
+    })
+
+    // CLOSE APP
+    ipc.on('closeApp', () => {
+      win.close()
+    })
 }
 
 require('electron-reload')(__dirname, {
---
+++ src/js/appFunctions.js
@@ -0,0 +1,34 @@
+const { ipcRenderer } = require('electron')
+const maxResBtn = document.querySelector('#buttonSquare')
+const ipc = ipcRenderer
+
+
+// MINIMAZI APP
+buttonRolleDown.addEventListener('click', () => {
+    ipc.send('minimazeApp')
+})
+
+// MINIMAZI RESTORE APP
+function changeMaxResBtn(isMaximizedApp){
+    if(isMaximizedApp){
+        maxResBtn.title = 'Restore'
+        document.querySelector('.MaximizeBtn img').setAttribute('src', 'img/restore.svg')
+    } else {
+        maxResBtn.title = 'Maximize'
+        document.querySelector('.MaximizeBtn img').setAttribute('src', 'img/square.svg')
+    }
+}
+buttonSquare.addEventListener('click', () => {
+    ipc.send('maximizeRestoreApp')
+})
+ipc.on('isMaximized', () => {
+    changeMaxResBtn(true)
+})
+ipc.on('isRestored', () => {
+    changeMaxResBtn(false)
+})
+
+// CLOSE APP
+document.querySelector('#buttonClose').addEventListener('click', ()=>{
+    ipc.send('closeApp')
+})
